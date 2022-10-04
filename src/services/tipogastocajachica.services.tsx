2d4447eb6837e457ace5de119740f9d8ce15ac59
Giovanni Orozco <80933087+giojodev@users.noreply.github.com>
2022-10-03 23:33:48 -0600

    Se agrega servicio de tipo gasto caja chica

---
+++ src/services/tipogastocajachica.services.tsx
@@ -0,0 +1,12 @@
+import axios from "axios";
+import { SessionData } from "../helpers";
+
+const GetBaseUrl=()=>{
+    return localStorage.getItem("urlAPIALAMEDAS");
+}
+
+class TipoGastoCajaChicaService{
+    
+}
+
+export {TipoGastoCajaChicaService};
