a8a76c157a5f2afba3bd35c26a030ae703046222
Giovanni Orozco <80933087+giojodev@users.noreply.github.com>
2022-10-05 23:45:15 -0600

    GetListTipoGastoCajaChica

--- src/services/tipogastocajachica.services.tsx
+++ src/services/tipogastocajachica.services.tsx
@@ -5,8 +5,25 @@ const GetBaseUrl=()=>{
     return localStorage.getItem("urlAPIALAMEDAS");
 }
 
+const urlBase=GetBaseUrl();
 class TipoGastoCajaChicaService{
     
+    static async GetListTipoGastoCajaChica(){
+        return new Promise((resolve,reject)=>{
+            const sessionData = SessionData() as IModelLoginRequest;
+            axios.get(urlBase + 'Alamedas/Catalog/GetTipoGastoCajaChica',{
+                headers:{
+                    'Authorization': `Bearer ${sessionData.token}` 
+                }
+            }).then(response=>{
+                resolve(response.data);
+            }).catch(error=>{
+                reject(error);
+                console.log("TipoGastoCajaChicaService.service.tsx ~ line 22 ~ TipoGastoCajaChicaService ~ GetListTipoGastoCajaChica ~ error",error);
+            })
+        })
+    }
+
 }
 
 export {TipoGastoCajaChicaService};
