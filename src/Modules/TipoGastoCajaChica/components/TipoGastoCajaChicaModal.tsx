b7c929eae135a9a0953f6732eeb4b29fbf9ff782
Giovanni Orozco <80933087+giojodev@users.noreply.github.com>
2022-10-17 00:38:03 -0600

    Avances en modal

--- src/Modules/TipoGastoCajaChica/components/TipoGastoCajaChicaModal.tsx
+++ src/Modules/TipoGastoCajaChica/components/TipoGastoCajaChicaModal.tsx
@@ -4,7 +4,17 @@ import {Modal,Input,InputNumber,Form,Row,Divider,Col,Radio, Button} from "antd";
 
 const TipoGastoCajaChicaModal =({showModal,formData,onChange,isEditData,onSave}: PropTipoGastoCajaChica)=>{
     return(
-        <></>
+        <>
+        <Modal visible={showModal} destroyOnClose={true} onCancel={onChange} title={isEditData ? "Editar Tipo Gasto Caja Chica" : "Nuevo Tipo Gasto Caja Chica"} footer={false} centered>
+            <Row justify='center'>
+                <Form layout='vertical' labelWrap labelCol={{span:12}} initialValues={formData} onFinish={onSave}>
+                    <Row gutter={[16,16]}>
+
+                    </Row>
+                </Form>
+            </Row>
+        </Modal>
+        </>
     );
 }
 
