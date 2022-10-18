dbff1eb2c479724bf670934486976fd0f948f1d6
Giovanni Orozco <80933087+giojodev@users.noreply.github.com>
2022-10-18 00:00:16 -0600

    Cambios Pagina Tipo Gasto Caja Chica

--- src/Modules/TipoGastoCajaChica/TipoGastoCajaChicaPage.tsx
+++ src/Modules/TipoGastoCajaChica/TipoGastoCajaChicaPage.tsx
@@ -2,8 +2,20 @@ import React,{useEffect,useState} from 'react';
 import { Button,Col,Divider,Row,Table,Modal,Input,Tag } from 'antd';
 import { TipoGastoCajaChicaService } from '../../services';
 import {TipoGastoCajaChicaModal} from './components';
+import { EditOutlined,LoadingOutlined,SaveOutlined,CloseCircleOutlined } from "@ant-design/icons";
+
+const Search= Input.Search;
+
 
 const   TipoGastoCajaChicaPage = ()=> {
+
+    const [lstTipoGastoCajaChica,setLstTipoGastoCajaChica] = useState([] as Array <IModelTipoGastoCajaChica>);
+    const [lstFilter,setLstFilter] = useState([] as Array<IModelTipoGastoCajaChica>);
+    const [loading,setLoading] = useState(false);
+    const [tipoGastoCajaChica,setTipoGastoCajaChica] = useState({}as IModelTipoGastoCajaChica);
+    const [isModalVisible,setIsModalVisible] = useState(false);
+    const [isEdit,setIsEdit] = useState(false);
+
     return(
 
 <></>
