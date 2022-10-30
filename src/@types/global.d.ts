4dc4f1b960d061efd7bc677da7b4808c7e6b428b
Giovanni Orozco <80933087+giojodev@users.noreply.github.com>
2022-10-30 16:49:58 -0600

    Cambio Tipado

--- src/@types/global.d.ts
+++ src/@types/global.d.ts
@@ -38,13 +38,13 @@ interface IModelLogin {
   }
 
   interface IModelTipoGastoCajaChica{
-    idGastoCajaChica:int = 0;
-    nombreGastoCajachica:string = "";
+    idTipoGastoCajaChica:int = 0;
+    nombreTipoGastoCajachica:string = "";
     activo:boolean = false;
   }
   interface IModelTipoIngresoCajaChica{
-    idIngresoaCajaChica:int = 0;
-    nombreIngresoCajaChica:string = "";
+    idTipoIngresoaCajaChica:int = 0;
+    nombreTipoIngresoCajaChica:string = "";
     activo:boolean = false;
   }
 
