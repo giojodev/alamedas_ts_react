0233deba06d5630de95408f39dd86374fb63e3e7
Giovanni Orozco <80933087+giojodev@users.noreply.github.com>
2022-11-06 19:44:26 -0600

    Update TipoGastoModal.tsx

--- src/Modules/TipoGasto/components/TipoGastoModal.tsx
+++ src/Modules/TipoGasto/components/TipoGastoModal.tsx
@@ -1,1 +1,14 @@
-import React from 'react';
+import {useState} from 'react';
+import { SaveOutlined,CloseCircleOutlined } from '@ant-design/icons';
+import {Modal,Input,InputNumber,Form,Row,Divider,Col,Radio, Button} from "antd";
+
+const TipoGastoModal =({showModal,formData,onChange,isEditData,onSave}: PropTipoIngreso)=>{
+    const spanCol : number = 12; 
+    const [currentValueRadio,setCurrentValueRadio] = useState(formData.activo);
+
+    return(
+        <></>
+    )
+}   
+
+export {TipoGastoModal};
