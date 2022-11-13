4c7c5bfa84919e87222218f9dee8d0f514a7b5f9
Giovanni Orozco <80933087+giojodev@users.noreply.github.com>
2022-11-13 11:21:19 -0600

    MODIFICACIONES A PAGE TIPO INGRESO

--- src/Modules/TipoIngreso/TipoIngresoPage.tsx
+++ src/Modules/TipoIngreso/TipoIngresoPage.tsx
@@ -1,11 +1,18 @@
 import React,{useEffect,useState} from 'react';
 import { Button,Col,Divider,Row,Table,Modal,Input,Tag } from 'antd';
-import { TipoIngresoCajaChicaService } from '../../services/tipoingresocajachica.services';
+import { TipoIngresoService } from '../../services/tipoingreso.services';
 import { TipoIngresoModal } from './components';
 import { EditOutlined,LoadingOutlined,SaveOutlined,CloseCircleOutlined } from "@ant-design/icons";
 import { useForm } from 'antd/es/form/Form';
 
 const Search = Input.Search;
-const TipoIngresoPage = () =>{}
+const TipoIngresoPage = () =>{
+    const [lstTipoIngreso, setLstTipoIngreso] = useState([] as Array<IModelTiPoIngreso>);
+    const [lstFilter,setLstFilter] = useState([] as Array<IModelTiPoIngreso>);
+    const [loading,setLoading] = useState(false);
+    const [tipoIngreso,setTipoIngreso] = useState({} as IModelTiPoIngreso);
+    const [isModalVisible,setIsModalVisible] = useState(false);
+    const [isEdit,setIsEdit] = useState(false);
+}
 
 export {TipoIngresoPage}
