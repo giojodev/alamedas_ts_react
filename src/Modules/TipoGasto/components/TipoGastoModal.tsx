c6fe8df3debabf3fe83b694a868f03d91ef7879e
Giovanni Orozco <80933087+giojodev@users.noreply.github.com>
2022-11-17 22:13:54 -0600

    Correccion dato en Modal Tipo Gasto

--- src/Modules/TipoGasto/components/TipoGastoModal.tsx
+++ src/Modules/TipoGasto/components/TipoGastoModal.tsx
@@ -2,7 +2,7 @@ import {useState} from 'react';
 import { SaveOutlined,CloseCircleOutlined } from '@ant-design/icons';
 import {Modal,Input,InputNumber,Form,Row,Divider,Col,Radio, Button} from "antd";
 
-const TipoGastoModal =({showModal,formData,onChange,isEditData,onSave}: PropTipoIngreso)=>{
+const TipoGastoModal =({showModal,formData,onChange,isEditData,onSave}: PropTipoGasto)=>{
     const spanCol : number = 12; 
     const [currentValueRadio,setCurrentValueRadio] = useState(formData.activo);
 
