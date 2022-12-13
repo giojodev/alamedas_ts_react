958d3af5c0118dfef6f5d9da634be26a35538863
Giovanni Orozco <80933087+giojodev@users.noreply.github.com>
2022-12-12 23:51:03 -0600

    Create GastosModal.tsx

---
+++ src/Modules/Gastos/components/GastosModal.tsx
@@ -0,0 +1,12 @@
+import { useState } from "react";
+import { SaveOutlined,CloseCircleOutlined } from "@ant-design/icons";
+import {Modal,Input,InputNumber,Form,Row,Divider,Col,Radio, Button} from "antd";
+import { PropGasto } from "../../../@types/global";
+
+const GastoModal =({showModal,formData,onChange,isEditData,onSave}: PropGasto)=>{
+const spanCol:number=12;
+// const [currentValueRadio,setCurrentValueRadio] = useState(formData.activo);
+
+}
+
+export {GastoModal};
