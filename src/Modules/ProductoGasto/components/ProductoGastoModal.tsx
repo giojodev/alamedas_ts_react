567add87a24759e275b9498fd84afa2b9a261c9e
Giovanni Orozco <80933087+giojodev@users.noreply.github.com>
2023-01-03 22:24:25 -0600

    Finalizado Modal Producto Gasto

--- src/Modules/ProductoGasto/components/ProductoGastoModal.tsx
+++ src/Modules/ProductoGasto/components/ProductoGastoModal.tsx
@@ -1,7 +1,54 @@
-import React from 'react'
+import {useState} from 'react'
+import { SaveOutlined,CloseCircleOutlined } from '@ant-design/icons';
+import {Modal,Input,InputNumber,Form,Row,Divider,Col, Button} from "antd";
 
-class ProductoGastoModal{
-    
+const ProductoGastoModal=({showModal,formData,onChange,isEditData,onSave}:PropProdGasto)=>{
+    return (
+        <>
+            <Modal visible={showModal} destroyOnClose={true} onCancel={onChange} title={isEditData ? "Editar Producto Gasto": "Nuevo Producto Gasto"} footer={false} centered>
+                <Form layout='vertical' labelWrap labelCol={{span:12}} initialValues={formData} onFinish={onSave}>
+                    <Col>
+                        <Form.Item
+                            label="IdEntity"
+                            name="IdEntity"
+                            hidden
+                            >
+                                <Input hidden/>
+                            </Form.Item>
+                    </Col>
+                    <Col>
+                        <Form.Item
+                            label="Concepto"
+                            name="Concepto"
+                            rules={[{required:true,message:"Ingrese el concepto"}]}
+                        >
+                                <Input/>
+                        </Form.Item>
+                    </Col>
+                    <Col>
+                    <Form.Item
+                            label="Valor"
+                            name="valor"
+                            rules={[{required:true,message:"Ingrese el valor"}]}
+                        >
+                                <InputNumber min={1} />
+                        </Form.Item>
+                    </Col>
+                    <Divider/>
+                    <Row justify="space-around">
+                        <Button type='default' onClick={onChange} icon={<CloseCircleOutlined/>}>
+                            Cerrar
+                        </Button>
+                        <Button type='primary' htmlType='submit' icon={<SaveOutlined/>}>
+                            Guardar
+                        </Button>
+                    </Row>
+                </Form>
+            </Modal>
+        </>
+    )
 }
+
+
 
 export {ProductoGastoModal};
