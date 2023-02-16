c5c3059d3fd3a406de9ba202fc133679a3a363ff
Giovanni Orozco <80933087+giojodev@users.noreply.github.com>
2023-02-15 22:35:40 -0600

    Cambios en pagina gasto caja chica

--- src/Modules/GastosCC/GastoCCPage.tsx
+++ src/Modules/GastosCC/GastoCCPage.tsx
@@ -1,2 +1,32 @@
 import React,{useEffect,useState} from 'react';
 import { Button,Col,Divider,Row,Table,Modal,Input,Tag } from 'antd';
+import { GastoCajaChicaService } from '../../services';
+import { EditOutlined,LoadingOutlined,SaveOutlined,CloseCircleOutlined } from "@ant-design/icons";
+import { ColumnsType } from 'antd/es/table';
+import { GastoCCModal } from './components';
+
+const Search= Input.Search;
+
+const GastoCCPage = () =>{
+
+    const [loading,setLoading] = useState(false);
+    const [lstGasto,stLstGasto] = useState([] as Array<IModelGastoCC>);
+    const [lstFilter,setLstFilter] = useState([] as Array<IModelGastoCC>);
+
+    const fetchListGastoCajaChica = async()=>{
+        var result= GastoCajaChicaService.GetListGastosCC();
+
+        result.then((data:any)=>{
+            stLstGasto(data);
+        })
+        .catch((error:any)=>{
+            Modal.error({
+                icon:<CloseCircleOutlined/>,
+                type:"error",
+                title:"Error",
+                content:error.response.data
+            });
+        }).finally(()=>{setLoading(false);});
+        
+    }
+}
